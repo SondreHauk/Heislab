abb141564cb3655e1de859041c1efb6c0b69d4d1
Sondre Holte <shholte@stud.ntnu.no>
2024-02-22 14:38:04 +0100

    modules added

---
+++ sceleton_project/driver/modules/queue.c
@@ -0,0 +1,10 @@
+#include "queue.h"
+
+
+QUEUE * queue_setup_maker(states * elev_states){
+    QUEUE * queue_new = (QUEUE *)malloc(sizeof(QUEUE));
+    
+    queue_new->current_floor = elev_states -> current_floor;
+
+
+};
